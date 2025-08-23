3ca6b8403e7b77eff4e8094143d0f595f081f847
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-08-23 17:14:36 -0400

    project files

---
+++ src/test/java/com/yuvam/student/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuvam.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
